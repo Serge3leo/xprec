59de42b16acc611c5f1bd9ebea3584e1671fa3d2
Serguei E. Leontiev <leo@sai.msu.ru>
2024-08-06 05:05:59 +0300

    Renamed hex->thex (tuple hex), hex() for future one hex float representation

--- test/test_repr.py
+++ test/test_repr.py
@@ -21,12 +21,12 @@ def xprec_fromfloats(hi, lo=None):
     return np.float64(hi).astype(xprec.ddouble) + np.float64(lo)
 
 
-def xprec_hex(x):
+def xprec_thex(x):
     hi, lo = xprec_floats(x)
     return f"({hi.hex()},{lo.hex()})"
 
 
-def xprec_fromhex(s):
+def xprec_fromthex(s):
     b = s.find('(')
     m = b + s[b:].find('+')
     if m < b:
@@ -51,7 +51,7 @@ def _unrepr(x):
     "(0x1.27ddbf6271dbep-4,-0x1.10535d4bf6190p-58)",
     ])
 def test_repr(h):
-    x = xprec_fromhex(h)
+    x = xprec_fromthex(h)
     assert x == _unrepr(repr(x))
 
 
@@ -63,12 +63,12 @@ def test_repr_random():
         assert x == _unrepr(repr(x))
 
 
-def test_hex_random():
+def test_thex_random():
     hi = np.random.uniform(-1., 1., size=1000)
     lo = np.random.uniform(-1., 1., size=hi.size)*np.finfo(np.float64).eps
     xs = xprec_fromfloats(hi, lo)
     for x in xs:
-        assert x == xprec_fromhex(xprec_hex(x))
+        assert x == xprec_fromthex(xprec_thex(x))
 
 
 def test_floats_random():
